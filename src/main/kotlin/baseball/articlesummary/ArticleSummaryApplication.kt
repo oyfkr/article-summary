be2342bd6455f5bdefd482ad0e07f144d091ac3b
오근혁 <oyfkr@naver.com>
2024-10-03 14:56:12 +0900

    [신규] 기사 요약 서버 첫 커밋

---
+++ src/main/kotlin/baseball/articlesummary/ArticleSummaryApplication.kt
@@ -0,0 +1,11 @@
+package baseball.articlesummary
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ArticleSummaryApplication
+
+fun main(args: Array<String>) {
+    runApplication<ArticleSummaryApplication>(*args)
+}
---
+++ src/test/kotlin/baseball/articlesummary/ArticleSummaryApplicationTests.kt
@@ -0,0 +1,13 @@
+package baseball.articlesummary
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ArticleSummaryApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
